69e1c2a6fbaf5aca18e9bc6b986cb6fbf2dd46cf
yukipapa1967 <tg.wara0810@gmail.com>
2024-07-28 08:07:41 +0900

    kadai_021 の提出

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+
+  setTimeout(() => {
+    console.log('ボタンをクリックしました');
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+  
+});
